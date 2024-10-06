206ae3514db2ccc619a18a58ff6393e0dc735908
Th3B3n1 <othersc9bst@gmail.com>
2024-10-06 13:50:21 +0200

    done

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+import express from 'express';
+import cors from 'cors';
+import 'dotenv';
+import multer from 'multer';
 
-var app = express();
+const app = express();
+const upload = multer();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,8 +13,10 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post("/api/fileanalyse", upload.single("upfile"), function (req, res)
+{
+  res.json({name: req.file.originalname, type: req.file.mimetype, size: req.file.size})
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
